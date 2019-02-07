4d9aa2c66aebc0b4ad3608995f5d176babb07e67
tom <tom@workstation.local>
2019-02-07 14:22:54 +0100

    some_del_crtlr

--- src/histo/ctrlr_action/back_in_history.c
+++ src/histo/ctrlr_action/back_in_history.c
@@ -24,23 +24,6 @@ void    print_all_value(t_42sh *sh, char *dup)
     ft_putchar('\n');
 }
 
-void    back_curs_dup(t_42sh *sh, char *arg, char *dup, int pos_line, int nb_moove)
-{
-    int i;
-
-    i = 0;
-    sh->stdin->len_line = ft_strlen(dup);
-    sh->history_mark->line_pos = pos_line;
-    sh->history_mark->cursor_pos = sh->prompt_len + pos_line;
-    sh->history_mark->nb_moove = nb_moove;
-    /*ft_puts_blue(" sh->history_mark->nb_moove  : ");
-    ft_putnbr( sh->history_mark->nb_moove );
-    ft_putchar('\n');
-    ft_putnbr(nb_moove);
-    */while(nb_moove-- > 0)
-        tputs(tgoto(tgetstr("le", NULL), 1, 0), 1, putchar_custom);
-}
-
 static int check_occ_in_dup(t_42sh *sh, char *arg, char *dup)
 {
     int i;
@@ -55,7 +38,6 @@ static int check_occ_in_dup(t_42sh *sh, char *arg, char *dup)
         if (arg[0] == dup[pos_start])
             if (check_after(arg, dup, pos_start, sh->history_mark->pos_arg) == 1)
             {
-                sh->stdin->len_line = ft_strlen(dup);
                 sh->history_mark->line_pos = pos_start;
                 sh->history_mark->cursor_pos = sh->prompt_len + pos_start;
                 sh->history_mark->nb_moove = i + (sh->stdin->len_line - sh->stdin->line_pos) + 1;
