608259f2f6c6ff3e9ff71a51d9cfdef08e0c4fea
ttresori <tresoriertom@gmail.com>
2019-02-08 14:38:04 +0100

    fc-lstrstr

--- src/builtin/fc/check_int_char.c
+++ src/builtin/fc/check_int_char.c
@@ -1,23 +1,5 @@
 #include "fc.h"
 
-int   check_if_digit(t_42sh *sh)
-{
-        if ((ft_isdigit(sh->argv->argv[2][0]) == 1) && sh->argv->argv[3] == NULL)    // -l 1 NULL
-           {
-                print_delimit_history(sh, ft_atoi(sh->argv->argv[2]),
-                sh->history_mark->size);
-                return (1);
-           }
-        if ((ft_isdigit(sh->argv->argv[2][0]) == 1)   // -l 1 2
-        && (ft_isdigit(sh->argv->argv[3][0]) == 1))
-        {
-            print_delimit_history(sh, ft_atoi(sh->argv->argv[2]),
-            ft_atoi(sh->argv->argv[3]));
-            return (1);
-        }
-    return (0);
-}
-
 static int get_int_stop_char(t_42sh *sh, char *stop_char, int len_stop)
 {
     int i;
@@ -33,6 +15,31 @@ static int get_int_stop_char(t_42sh *sh, char *stop_char, int len_stop)
         i++;
     }
     return (-1);
+}
+
+int   check_if_digit(t_42sh *sh)
+{
+        if ((ft_isdigit(sh->argv->argv[2][0]) == 1) && sh->argv->argv[3] == NULL)    // -l 1 NULL
+           {
+                print_delimit_history(sh, ft_atoi(sh->argv->argv[2]),
+                sh->history_mark->size);
+                return (1);
+           }
+        else if ((ft_isdigit(sh->argv->argv[2][0]) == 1)   // -l 1 2
+        && (ft_isdigit(sh->argv->argv[3][0]) == 1))
+        {
+            print_delimit_history(sh, ft_atoi(sh->argv->argv[2]),
+            ft_atoi(sh->argv->argv[3]));
+            return (1);
+        }
+        if ((ft_isdigit(sh->argv->argv[2][0]) == 1)   // -l 1 2
+        && (ft_isalpha(sh->argv->argv[3][0]) == 1))
+        {
+            print_delimit_history(sh, ft_atoi(sh->argv->argv[2]),
+            get_int_stop_char(sh, sh->argv->argv[3], ft_strlen(sh->argv->argv[3])));
+            return (1);
+        }
+    return (0);
 }
 
 static void print_delimit_history_str(t_42sh *sh, char *arg_start, char *stop, int start_int, int stop_int)
